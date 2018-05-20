18bfe1f32f5d03a307c24b32849f2e8984ddd35e
Ryan Ghaida <ryaboug@gmail.com>
2018-05-20 13:08:07 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,50 @@
 Rails.application.routes.draw do
+
+  resources :songs
+  resources :genres
+  resources :artists
+  # get 'song/new'
+  #
+  # get 'song/create'
+  #
+  # get 'song/edit'
+  #
+  # get 'song/update'
+  #
+  # get 'song/destroy'
+  #
+  # get 'song/index'
+  #
+  # get 'song/show'
+  #
+  # get 'genre/new'
+  #
+  # get 'genre/create'
+  #
+  # get 'genre/edit'
+  #
+  # get 'genre/update'
+  #
+  # get 'genre/destroy'
+  #
+  # get 'genre/index'
+  #
+  # get 'genre/show'
+  #
+  # get 'artist/new'
+  #
+  # get 'artist/create'
+  #
+  # get 'artist/edit'
+  #
+  # get 'artist/update'
+  #
+  # get 'artist/destroy'
+  #
+  # get 'artist/index'
+  #
+  # get 'artist/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
